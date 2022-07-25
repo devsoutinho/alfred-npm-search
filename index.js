ff13d16effe5e63acad18ae28d1290e9d073b607
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-07-25 09:31:11 -0300

    update

--- index.js
+++ index.js
@@ -4,18 +4,22 @@ import alfy from 'alfy';
 const input = alfy.input;
 
 // [PROCESS]
-const packages = [
-	{
-		name: 'facebook/react',
-	},
-	{
-		name: '@skynexui/components',
-	}
-];
-const output = packages.map((pkg) => ({
-    url: 'https://google.com',
-	title: `${pkg.name} ${input}`
-}));
+const body = await alfy.fetch(`https://api.skypack.dev/v1/search?q=${input}`);
+
+const packagerURL = "http://www.npmjs.com/packagex";
+const packages = body.results.map((item) => {
+	return {
+		title: `${item.name}`,
+		subtitle: `${item.description} ${item.hasTypes ? "(TS ready)" : ""}`,
+		arg: `${packagerURL}/${item.name}`,
+		text: {
+			copy: `${packagerURL}/${item.name}`,
+			largetype: `${packagerURL}/${item.name}`,
+		},
+	};
+});
+
 
 // [OUTPUT]
+const output = packages.map((pkg) => pkg);
 alfy.output(output);
